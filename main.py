d46ba6c2f82ba42d68eed68fff7cc8e4c325e841
Al Riyan <alriyankhan7678@gmail.com>
2026-09-13 20:24:12 +0530

    add till vidoe capture

--- main.py
+++ main.py
@@ -1,32 +1,75 @@
 import cv2
 import mediapipe as mp
+import os
+import argparse
 
-# read image
-img_path = 'data/faceImage.jpg'
-img = cv2.imread(img_path)
+output_dir = 'output'
 
-H,W, _ = img.shape
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+
+def process_img(img,face_detection):
+
+    H,W,_ = img.shape
+    img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    out = face_detection.process(img_rgb)
+    
+    if out.detections is not None:
+        for detection in out.detections:
+            location_data = detection.location_data
+            bbox = location_data.relative_bounding_box
+    
+            x1,y1,w,h = bbox.xmin,bbox.ymin,bbox.width,bbox.height
+    
+            x1 = int(x1*W)
+            y1 = int(y1*H)
+            w = int(w*W)
+            h = int(h*H)
+    
+            # img = cv2.rectangle(img,(x1,y1),(x1+w,y1+h),(0,255,0),10)
+    
+            # blur image
+            img[y1:y1+h,x1:x1+w,:] = cv2.blur(img[y1:y1+h,x1:x1+w,:],(30,30))
+
+    return img
+
+args = argparse.ArgumentParser()
+
+args.add_argument("--mode",default='video')
+args.add_argument('--filepath',default='data/testVideo.mp4')
+
+args = args.parse_args()
 
 # detect faces
 mp_face_detections = mp.solutions.face_detection
 
 with mp_face_detections.FaceDetection(model_selection=0,min_detection_confidence=0.5) as face_detection:
-    img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    out = face_detection.process(img_rgb)
 
-    if out.detections is not None:
-        for detection in out.detections:
-            location_data = detection.location_data
-            bbox = location_data.relative_bounding_box
+    if args.mode in ['image']:
+        img = cv2.imread(args.filepath)
 
-            x1,y1,w,h = bbox.xmin,bbox.ymin,bbox.width,bbox.height
+        img = process_img(img,face_detection)
 
-            x1 = int(x1*W)
-            y1 = int(y1*H)
-            w = int(w*W)
-            h = int(h*H)
+        #save img
+        cv2.imwrite(os.path.join(output_dir,'output.png'),img)
 
-            img = cv2.rectangle(img,(x1,y1),(x1+w,y1+h),(0,255,0),10)
+    if args.mode in ['video']:
 
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
+        cap = cv2.VideoCapture(args.filepath)
+        ret,frame = cap.read()
+
+        output_video = cv2.VideoWriter(os.path.join(output_dir,'output.mp4'),
+                                       cv2.VideoWriter_fourcc(*'MP4V'),
+                                       25,
+                                       (frame.shape[1],frame.shape[0]))
+
+        while ret:
+            frame = process_img(frame,face_detection)
+
+            output_video.write(frame)
+            ret,frame = cap.read()
+
+        cap.release()
+        output_video.release()
+        
